86c5df0e81f32937885ecc4527e9140d48c3912d
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-11 16:19:03 +0300

    Add a loop game

---
+++ Section 11 Javascript Basics Control/Annoy-omatic/index.js
@@ -0,0 +1,8 @@
+let answer = prompt('Are we there yet!')
+
+while(answer!== "yes" && answer.indexOf('yes')===-1){
+    prompt('Are we there yet!')
+}
+
+alert('YAY') //this cant showed on screen until while loop is stopped
+

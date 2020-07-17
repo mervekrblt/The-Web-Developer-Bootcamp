f7f307758c1ba2ad4f722c0f5ea801497662d1d3
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-17 21:16:20 +0300

    Add delete feature for todo exercise

--- Section 13 Arrays/ToDo list/index.js
+++ Section 13 Arrays/ToDo list/index.js
@@ -5,12 +5,20 @@ let input = prompt('What would you like to do?')
 while(input !== 'quit'){
     
     if(input==='list'){
-    console.log(todos)
+      todos.forEach(function(todo, b){
+        console.log(`${b} : ${todo}`)
+      })
 }else if(input==='new'){
 
 let  newtodo = prompt('Add new to do')
 todos.push(newtodo)
+} else if(input === 'delete'){
+  let input = prompt('Enter index of todo to delete?')
+  todos.splice(index,1)
 }
+
+
+
   input = prompt('What would you like to do?')
 }
 

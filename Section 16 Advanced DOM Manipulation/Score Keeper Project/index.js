0050a55a3047c929cfd60980e5d4d9e4134d7d22
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-30 21:08:37 +0300

    Add reset functionality

--- Section 16 Advanced DOM Manipulation/Score Keeper Project/index.js
+++ Section 16 Advanced DOM Manipulation/Score Keeper Project/index.js
@@ -1,6 +1,8 @@
 const p1 = document.querySelector("#p1")
 const p2 = document.querySelector("#p2")
 const reset = document.querySelector("#reset")
+const number = document.querySelector("#addNum")
+const playingNum = document.querySelector("#plyngnum")
 
 let p1Score = 0
 let p2Score = 0
@@ -13,7 +15,7 @@ p1.addEventListener("click", function addScore() {
 
     if (!gameOver) {
         p1Score++;
-        if (p1Score === winningScore) {   // this statement means that p1 is winner, the code about changing color have to take part in here 
+        if (p1Score === winningScore) { // this statement means that p1 is winner, the code about changing color have to take part in here 
             scr1.classList.add('winner')
             gameOver = true // !!!!!!  this lie of code makes !gameOver is false so this code is stopped to add number. Also gameOver is same for p2, p2 is stopped to add number 
         }
@@ -34,4 +36,25 @@ p2.addEventListener("click", function addScore() {
         }
     }
     scr2.textContent = p2Score
+})
+
+
+reset.addEventListener("click", function () {
+    resett()
+})
+
+function resett(){
+    scr1.textContent = 0
+    scr2.textContent = 0
+    p1Score = 0
+    p2Score = 0
+    scr1.classList.remove("winner")
+    scr2.classList.remove("winner")
+    gameOver = false
+}
+
+number.addEventListener("change", function(){ //new event change
+    playingNum.textContent = Number (this.value)
+    winningScore = Number(this.value)
+    resett()
 })

789860b42f4a64927860fc92890b8ad3c2aa34e7
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-30 20:00:45 +0300

    Add an if condition in eventlisteners

--- Section 16 Advanced DOM Manipulation/Score Keeper Project/index.js
+++ Section 16 Advanced DOM Manipulation/Score Keeper Project/index.js
@@ -5,14 +5,32 @@ const reset = document.querySelector("#reset")
 let p1Score=0
 let p2Score=0
 
+let gameOver= false //should use let because this boolean will be changed
+let winningScore =5
+
 
 p1.addEventListener("click",function addScore(){
-    p1Score++;
+
+    if(!gameOver){
+        p1Score++;
+            if(p1Score === winningScore){
+                gameOver = true  // !!!!!!  this lie of code makes !gameOver is false so this code is stopped to add number. Also gameOver is same for p2, p2 is stopped to add number 
+            }
     scr1.textContent = p1Score
+    }
+
+
+    
 })
 
 p2.addEventListener("click",function addScore(){
-    p2Score++;
+    
+    if(!gameOver){
+        p2Score++;
+            if(p2Score === winningScore){
+                gameOver = true
+            }
+    }
     scr2.textContent = p2Score
 })
 

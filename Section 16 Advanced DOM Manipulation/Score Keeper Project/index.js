ad4fb100f7f6c24e3194325e12e673ddc6749089
Merve Karabulut <mervekarabulut@lonca.works>
2020-07-30 20:16:33 +0300

    Change score color with adding a class, DOM

--- Section 16 Advanced DOM Manipulation/Score Keeper Project/index.js
+++ Section 16 Advanced DOM Manipulation/Score Keeper Project/index.js
@@ -2,35 +2,36 @@ const p1 = document.querySelector("#p1")
 const p2 = document.querySelector("#p2")
 const reset = document.querySelector("#reset")
 
-let p1Score=0
-let p2Score=0
+let p1Score = 0
+let p2Score = 0
 
-let gameOver= false //should use let because this boolean will be changed
-let winningScore =5
+let gameOver = false //should use let because this boolean will be changed
+let winningScore = 5
 
 
-p1.addEventListener("click",function addScore(){
+p1.addEventListener("click", function addScore() {
 
-    if(!gameOver){
+    if (!gameOver) {
         p1Score++;
-            if(p1Score === winningScore){
-                gameOver = true  // !!!!!!  this lie of code makes !gameOver is false so this code is stopped to add number. Also gameOver is same for p2, p2 is stopped to add number 
-            }
-    scr1.textContent = p1Score
+        if (p1Score === winningScore) {   // this statement means that p1 is winner, the code about changing color have to take part in here 
+            scr1.classList.add('winner')
+            gameOver = true // !!!!!!  this lie of code makes !gameOver is false so this code is stopped to add number. Also gameOver is same for p2, p2 is stopped to add number 
+        }
+        scr1.textContent = p1Score
     }
 
 
-    
+
 })
 
-p2.addEventListener("click",function addScore(){
-    
-    if(!gameOver){
+p2.addEventListener("click", function addScore() {
+
+    if (!gameOver) {
         p2Score++;
-            if(p2Score === winningScore){
-                gameOver = true
-            }
+        if (p2Score === winningScore) {
+            scr2.classList.add('winner')
+            gameOver = true
+        }
     }
     scr2.textContent = p2Score
 })
-

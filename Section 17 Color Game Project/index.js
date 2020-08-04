4ef5bf887628d82d23e6defcaa7a9bc2a02edd83
Merve Karabulut <mervekarabulut@lonca.works>
2020-08-04 20:22:50 +0300

    Change square's colors when click the reset button

--- Section 17 Color Game Project/index.js
+++ Section 17 Color Game Project/index.js
@@ -4,6 +4,22 @@ let squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
 let colorDisplay = document.querySelector('#colorDisplay');
 let message = document.querySelector('#message');
+let h1 = document.querySelector("h1")
+let resetButton = document.querySelector("#reset")
+
+resetButton.addEventListener("click", function(){
+    //generate all new colors
+    colors = generateRandomColors(6)
+    // pick a new random color from array
+    pickedColor = pickColor()
+    //change colorDisplay
+    colorDisplay.textContent = pickedColor
+    //change colors of squares
+    squares.forEach(function (i, j, ) {
+        i.style.backgroundColor = colors[j]
+    })
+    h1.style.backgroundColor = "#232323"  // when click the button h1 color returns to normal
+})
 
 
 colorDisplay.textContent = pickedColor
@@ -21,6 +37,8 @@ squares.forEach(function (i, j, ) {
         if (clikcedColor === pickedColor) {
             message.textContent = "Correct"
             changeColors(pickedColor) //implemented
+            h1.style.backgroundColor=pickedColor
+            resetButton.textContent = "Play Again"
         } else {
             this.style.backgroundColor = "#232323" // fade out color code and this refers to squares
             message.textContent = "Try Again"

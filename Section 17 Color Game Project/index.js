2275c5904bcf20d0db920d8a420bda5ed09c18e8
Merve Karabulut <mervekarabulut@lonca.works>
2020-08-05 00:14:36 +0300

    Add easy and hard mode & show 3 squares for easy, 6 squares for hard mode

--- Section 17 Color Game Project/index.js
+++ Section 17 Color Game Project/index.js
@@ -1,4 +1,5 @@
-let colors = generateRandomColors(6)
+var numSquares =6
+let colors = generateRandomColors(numSquares)
 
 let squares = document.querySelectorAll('.square');
 let pickedColor = pickColor();
@@ -6,10 +7,50 @@ let colorDisplay = document.querySelector('#colorDisplay');
 let message = document.querySelector('#message');
 let h1 = document.querySelector("h1")
 let resetButton = document.querySelector("#reset")
+let easyBtn = document.querySelector("#easyBtn")
+let hardBtn = document.querySelector("#hardBtn")
+
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numSquares =3;
+    colors = generateRandomColors(numSquares)
+    pickedColor =pickColor()
+    colorDisplay.textContent = pickedColor
+    colorDisplay.textContent = pickedColor
+
+    for(let i=0; i< squares.length; i++){ // use squares.length instead of colors.length 
+        if(colors[i]){
+            squares[i].style.backgroundColor = colors[i]
+        }else{
+            squares[i].style.display= "none"
+        }
+    }
+
+
+    
+})
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected")
+    numSquares=6;
+    colors = generateRandomColors(numSquares)
+    pickedColor =pickColor()
+    colorDisplay.textContent = pickedColor
+    colorDisplay.textContent = pickedColor
+
+    for(let i=0; i< squares.length; i++){ // use squares.length instead of colors.length 
+
+        squares[i].style.backgroundColor = colors[i]
+        squares[i].style.display= "block"
+    }
+
+})
 
 resetButton.addEventListener("click", function(){
     //generate all new colors
-    colors = generateRandomColors(6)
+    colors = generateRandomColors(numSquares)
     // pick a new random color from array
     pickedColor = pickColor()
     //change colorDisplay

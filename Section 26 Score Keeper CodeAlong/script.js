ed5117ac10746e4d7cbdf1e4febeb12d972930b9
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-04 18:56:21 +0300

    Add keep score example

--- Section 26 Score Keeper CodeAlong/script.js
+++ Section 26 Score Keeper CodeAlong/script.js
@@ -1,25 +1,52 @@
 const btn1 = document.querySelector('#btn1')
 const btn2 = document.querySelector('#btn2')
-const input = document.querySelector('#input')
 const p1 = document.querySelector('#p1')
-const reset = document.querySelector('#reset')
+const p2= document.querySelector('#p2')
+const resetButton = document.querySelector('#resetButton')
+const input = document.querySelector('input[type=number]')
 
-let count = 0
-let winnerScore = 5
+let count1 = 0
+let count2 = 0
+let winnerScore = 0
 let isGameOver = false
 
-btn1.addEventListener('click', function () {
+//for player1
+
+btn1.addEventListener('click',  () => {
     if (!isGameOver) {
-        count++
-        if (count === winnerScore) {
+        count1+=1
+        if (count1 === winnerScore) {
             isGameOver = true
         }
-        p1.innerHTML = count
+        p1.innerHTML = count1
     }
 })
 
-reset.addEventListener('click', function(){
+//for player2
+btn2.addEventListener('click', ()=>{
+    if(!isGameOver){
+        count2+=1
+        if(count2===winnerScore){
+            isGameOver= true
+        }
+        p2.innerHTML =count2
+    }
+})
+
+const reset = () =>{
     isGameOver = false
     count=0
     p1.innerHTML = 0
+    p2.innerHTML =0
+    count1=0
+    count2 = 0
+}
+
+resetButton.addEventListener('click', reset)
+
+input.addEventListener('change', (e) =>{
+console.log(e.target.valueAsNumber) // I found this propery from console
+
+winnerScore = e.target.valueAsNumber
+reset()// reset the all things
 })
